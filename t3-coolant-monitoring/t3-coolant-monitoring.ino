9086c6ec41f973241bfc40b5ebf4685aca285848
Marco Stixenberger <marco.stixenberger@gmail.com>
2021-05-14 20:00:01 +0200

    Update t3-coolant-monitoring.ino

--- t3-coolant-monitoring/t3-coolant-monitoring.ino
+++ t3-coolant-monitoring/t3-coolant-monitoring.ino
@@ -7,21 +7,21 @@
 #include <SPI.h>
 
 #define ONE_WIRE_BUS 2
+#define buzzer_pin 5
 #define Anzahl_Sensoren_DS18B20 2 // Anzahl der angeschlossenen Sensoren - Mögliche Werte: '0','1','2',... 
-
-
-
 #define cs 10
 #define dc 9
 #define rst 8
 
 #define DS18B20_Aufloesung 12
-DeviceAddress DS18B20_Adressen;
 
+DeviceAddress DS18B20_Adressen;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature myDS18B20(&oneWire);
 TFT TFTscreen = TFT(cs, dc, rst);
 
+
+//GLOBAL VARIABLES---------------------------------------------------------------------------------------------
 
 
 const float No_Val = 999.99;
@@ -34,6 +34,7 @@ unsigned int runtime = 0;
 unsigned int seconds = 0;
 unsigned int minutes = 0;
 unsigned int hours = 0;
+unsigned int num_cycles = 0;
 
 unsigned int temp_color_red_1;
 unsigned int temp_color_red_2;
@@ -43,6 +44,23 @@ unsigned int temp_color_green_2;
 String temp1_string_old;
 String temp2_string_old;
 
+
+//CUSTOM FUNCTIONS---------------------------------------------------------------------------------------------
+
+
+void alarm_beep(){
+  tone(buzzer_pin, 400);
+  delay(200);
+  noTone(buzzer_pin);
+  }
+
+
+void flash_screen(){
+  TFTscreen.background(255, 0, 0);
+  delay(20);
+  TFTscreen.background(0, 0, 0);
+  }
+//SETUP---------------------------------------------------------------------------------------------
 
 void setup(void) {
   TFTscreen.begin();
@@ -75,6 +93,9 @@ void setup(void) {
 }
 
 
+//LOOP---------------------------------------------------------------------------------------------
+
+
 void loop(void) {
   runtime = millis() * 1000;
   hours = runtime;
@@ -102,10 +123,10 @@ void loop(void) {
 
   temp_color_red_1 = map(temperature[0], 22, 35, 0, 255);
   temp_color_red_2 = map(temperature[1], 22, 35, 0, 255);
-
   temp_color_green_1 = map(temperature[0], 22, 35, 255, 0);
   temp_color_green_2 = map(temperature[1], 22, 35, 255, 0);
 
+/*
   Serial.print("red_1: ");
   Serial.print(temp_color_red_1);
   Serial.print("\n");
@@ -118,9 +139,13 @@ void loop(void) {
   Serial.print("green_2: ");
   Serial.print(temp_color_green_2);
   Serial.print("\n");
+*/
 
-
-
+  if (((abs(temperature[0]-temperature[1])) > 2) && (num_cycles % 5) ==0){
+    flash_screen();
+    alarm_beep();
+    } 
+  
   temp1_string = String(temperature[0]);
   temp1_string.toCharArray(temp1_printout, 6);
   temp2_string = String(temperature[1]);
@@ -146,5 +171,6 @@ void loop(void) {
   temp1_string_old = temp1_string;
   temp2_string_old = temp2_string;
 
+  num_cycles++;
   delay(1000);
 }
